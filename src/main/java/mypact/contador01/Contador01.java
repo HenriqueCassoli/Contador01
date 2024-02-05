884112f084533f6865de5b4264b6e31538211fa5
Cassoli <fernandes.cassoli@gmail.com>
2024-02-05 13:47:42 -0300

    first commit

---
+++ src/main/java/mypact/contador01/Contador01.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package mypact.contador01;
+
+/**
+ *
+ * @author Pessoal
+ */
+public class Contador01 {
+
+    public static void main(String[] args) {
+        
+        int cc = 0;
+        while (cc < 10){
+            cc++;
+            if (cc == 2 || cc == 4 ){
+                continue;
+            }
+            if(cc == 6){
+                break;
+            }
+            System.out.println("Cambalhota "+ cc);
+            
+        }
+    }
+}
